24499bbb1ab65c5390585ecb5967f5007a0bda72
ugo zanzi <ugo.zanzi@gmail.com>
2023-03-27 00:35:36 +0200

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -26,14 +26,6 @@ const router = createRouter({
       component: () => import('../views/Profil.vue')
     },
     {
-      path: '/contact',
-      name: 'contact',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Contact.vue')
-    },
-    {
       path: '/blog',
       name: 'blog',
       // route level code-splitting
